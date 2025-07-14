fe909edae39197358948fd18d50907de5119e157
Ru_Ru24 <rubear24@outlook.com>
2025-07-14 12:55:56 -0500

    Initial Commit

---
+++ CosmosConnect/CosmosConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmosConnectApp.swift
+//  CosmosConnect
+//
+//  Created by Scholar on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct CosmosConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
